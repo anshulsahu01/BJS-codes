82dc6c669204e3d4adcb6ec7b5fd7c8ee4a84ce3
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-27 16:20:16 +0530

    datatype

---
+++ JavaScript-codes/02dataType/index.js
@@ -0,0 +1,27 @@
+/*
+there are two type f data typw in js
+
+
+1. Primitive data type
+   a. Number 
+   b. String
+   c. Boolean
+   d. Undefine
+   e. null
+
+2.Non primitive data type
+  1.object
+  2.Arry
+
+*/
+
+// Number 
+
+var num1= 100;
+console.log(num1);
+
+var num2= 200;
+console.log(num2);
+
+var result = num1+num2
+console.log("the result is "+result)
--- JavaScript-codes/first js code/first code in js/Script.js
+++ JavaScript-codes/first js code/first code in js/Script.js
@@ -1,12 +1,48 @@
-
+/* let , var , const */
 //  declaration+ assignment 
 
 // variable declation hamesha small letter se hi krna hai jese var not Var , let not Let
+
+
 var firstName = "anshul";
 
 // declaration 
 let lastName = "sahu "
 
+
+console.log(firstName);
+console.log(lastName);
+
+var firstName = "raju";
 console.log(firstName);
 
-console.log(lastName);
+
+
+let phoneNumber = 75829898;
+
+console.log("the number is " +phoneNumber);
+
+// let phoneNumber = 998138860;
+console.log("the number is " +phoneNumbe);
+// let or var diff 
+
+/*
+In JavaScript, let is block-scoped, 
+meaning it's limited to the block (or function) where it's declared,
+ while var is function-scoped, allowing access throughout the entire function, 
+ which can lead to unexpected behavior and bugs. Use let for safer variable scoping.
+ */
+
+ console.log('');
+
+/*
+--variable declared in js must be unique.
+--the variable names can contain lette digits underscore 
+--variable name cannnot be reserved keyword .
+--cariable are also case sensitive  
+*/
+
+
+const anshul = admin;
+
+console.log(anshul);
