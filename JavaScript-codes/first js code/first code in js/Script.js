cb32f4fafbcd93493d734b7568c48e1f4a55e9ad
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-27 14:43:15 +0530

    Variable declartion

---
+++ JavaScript-codes/first js code/first code in js/Script.js
@@ -0,0 +1,8 @@
+
+//  declaration+ assignment 
+
+// variable declation hamesha small letter se hi krna hai jese var not Var , let not Let
+var firstName = "anshul";
+
+// declaration 
+let lastName = "sahu "
