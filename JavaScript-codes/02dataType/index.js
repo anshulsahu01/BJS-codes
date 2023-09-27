9195a2c75278cf28b2764870e11f1992a9434f7c
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-27 17:00:12 +0530

    types

--- JavaScript-codes/02dataType/index.js
+++ JavaScript-codes/02dataType/index.js
@@ -22,13 +22,51 @@ console.log(num1);
 console.log(typeof num1);
 
 
-var num1= "100";
-console.log(num1);
-console.log(typeof num1);
 
 
-var num2= 200;
+var num2= 20.0;
 console.log(num2);
 
 var result = num1+num2
 console.log("the result is "+result)
+
+
+// Sting
+
+var num1= "100";
+console.log(num1);
+console.log(typeof num1);
+
+var num= "200";
+console.log(num1);
+console.log(typeof num1);
+
+
+// Boolean
+
+var num1= true;
+console.log(num1);
+console.log(typeof num1);
+
+var num2= false;
+console.log(num2);
+console.log(typeof num1);
+
+
+//undefined
+
+var city= 
+console.log(city);
+console.log(typeof city);
+
+
+// null
+/* if the element is not present and we are trying to acess then it will return null
+*/
+var ref = null;
+console.log(ref);
+console.log(typeof ref);
+
+
+
+
