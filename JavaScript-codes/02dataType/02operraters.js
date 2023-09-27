f4eee87bf6501828960907bbebca446245733d5d
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-27 18:01:30 +0530

    +,-,*,/,%,++,--,+=,-=

---
+++ JavaScript-codes/02dataType/02operraters.js
@@ -0,0 +1,59 @@
+// Arithemetic
+
+// addition
+
+let a =5;
+let b =15;
+let addition = a=+b;
+console.log(addition);
+
+// sub
+var c=8;
+var d=10;
+var result=c-d;
+console.log(result);
+
+
+// multiply
+var c=8;
+var d=10;
+var result=c*d;
+console.log(result);
+
+
+// divide
+
+var c=8;
+var d=10;
+var result=c/d;
+console.log(result);
+
+//modulo
+
+var c=8;
+var d=10;
+var result=c%d;
+console.log(result);
+
+
+
+
+// Increment
+
+var data = 5;
+data++;
+console.log(data);
+
+data +=2;
+console.log(data);
+
+
+//Decrement
+
+var num = 54
+num--
+console.log(num);
+
+
+num-=4
+console.log(num);
