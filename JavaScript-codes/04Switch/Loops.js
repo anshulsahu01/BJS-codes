e346f87b62e064907ea828e43d32e97ce830e4ea
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-04 09:25:18 +0530

    switch an loop

---
+++ JavaScript-codes/04Switch/Loops.js
@@ -0,0 +1,5 @@
+// for loop 
+
+for(let i=0;i<=4;i++){
+    console.log("hey ");
+}
---
+++ JavaScript-codes/04Switch/Scipt.js
@@ -0,0 +1,16 @@
+// var choice = prompt ("enteryour choice")
+var choice = "Sachin"
+console.log(choice);
+
+switch(choice){
+    case 'anshul':
+        console.log(choice +'math score is 95');
+        break;
+    case 'Sachin':
+        console.log(choice +'math score is 85');
+        break;
+    case 'raj':
+        console.log(choice +'math score is 75');
+        break;
+
+}
