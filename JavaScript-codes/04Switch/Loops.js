008c862eb09425b91dc3236021cf83aa3dac581f
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-04 10:18:36 +0530

    while loop

--- JavaScript-codes/04Switch/Loops.js
+++ JavaScript-codes/04Switch/Loops.js
@@ -1,5 +1,18 @@
 // for loop 
-
+/*
 for(let i=0;i<=4;i++){
-    console.log("hey ");
-}
+    console.log(i);
+}*/
+
+// var firstname =`anshul`
+// console.log(`the value=$(firstname)`);
+
+
+// While loop
+
+var a=10 ;
+while(a<=20){
+    console.log("inside while "+a);
+
+    a+=2
+} console.log("outside while");
