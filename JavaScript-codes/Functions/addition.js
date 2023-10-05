5e359317f2419d61c9c0eac6f88aa85f73859763
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-05 09:03:07 +0530

    defination

---
+++ JavaScript-codes/Functions/addition.js
@@ -0,0 +1,21 @@
+/* Function 
+1) in js fun are used to perform certain operation
+
+Advantageges of fun
+'code re-useablity'
+
+1)(you can call the fun n number of time)
+2)less coding 
+3) we can re-use the same line of code multiple time to perform the common 
+4) fun in js will be return using the funtion keyword 
+5) the fun call =ing always include the ()
+you can call the fun before fun defination or after fun defination
+
+Ex:- function funtion_name(){
+
+body
+
+}
+*/
+//Write the code to perform the addition operation 
+
