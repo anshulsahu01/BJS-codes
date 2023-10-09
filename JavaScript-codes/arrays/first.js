23a7090a83c3c77d519647b1a963fa69e0f25636
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-09 10:15:29 +0530

    push pop

--- JavaScript-codes/arrays/first.js
+++ JavaScript-codes/arrays/first.js
@@ -1,37 +1,46 @@
-// syntex
+// // syntex
 
-// var arr =[ ]
-// console.log(arr);
-
-
-// example 1: empty array
-var ar =[]
-console.log(ar);
+// // var arr =[ ]
+// // console.log(arr);
 
 
-
-// ex2 Declaring an array of number data type
-var num =[12,13,14,15,16,]
-// displaying using reference
-console.log(num);
-
-// displaying using reference
-console.log(num[0]);
-console.log(num[1]);
-console.log(num[2]);
-console.log(num[3]);
-console.log(num[4]);
-
-// Ex declare an array of string data type
-
-var fullname=['ansh','chirag','bobby','janu','sashi',['a','b']]
-console.log(fullname);
+// // example 1: empty array
+// var ar =[]
+// console.log(ar);
 
 
-// ex;- 3 heterogenous datatype 
-var anshuldetails=[1,'anshuu','marks',20,14,'hello']
-console.log(anshuldetails);
-for(let i=1;i<=anshuldetails.length;i++){
-   console.log(i+" "+anshuldetails[i]);
 
-}
+// // ex2 Declaring an array of number data type
+// var num =[12,13,14,15,16,]
+// // displaying using reference
+// console.log(num);
+
+// // displaying using reference
+// console.log(num[0]);
+// console.log(num[1]);
+// console.log(num[2]);
+// console.log(num[3]);
+// console.log(num[4]);
+
+// // Ex declare an array of string data type
+
+// var fullname=['ansh','chirag','bobby','janu','sashi',['a','b']]
+// console.log(fullname);
+
+
+// // ex;- 3 heterogenous datatype 
+// var anshuldetails=[1,'anshuu','marks',20,14,'hello']
+// console.log(anshuldetails);
+// for(let i=1;i<=anshuldetails.length;i++){
+//    console.log(i+" "+anshuldetails[i]);
+
+// }
+
+let arr=['a','b','c','d']
+//push
+arr.push=('e')
+console.log(arr);
+
+// pop
+arr.pop()
+console.log(arr);
