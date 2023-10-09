2c2fe781dacc534a63cc9e6325871a9f3044d13a
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-09 09:53:45 +0530

    arrays

---
+++ JavaScript-codes/arrays/first.js
@@ -0,0 +1,37 @@
+// syntex
+
+// var arr =[ ]
+// console.log(arr);
+
+
+// example 1: empty array
+var ar =[]
+console.log(ar);
+
+
+
+// ex2 Declaring an array of number data type
+var num =[12,13,14,15,16,]
+// displaying using reference
+console.log(num);
+
+// displaying using reference
+console.log(num[0]);
+console.log(num[1]);
+console.log(num[2]);
+console.log(num[3]);
+console.log(num[4]);
+
+// Ex declare an array of string data type
+
+var fullname=['ansh','chirag','bobby','janu','sashi',['a','b']]
+console.log(fullname);
+
+
+// ex;- 3 heterogenous datatype 
+var anshuldetails=[1,'anshuu','marks',20,14,'hello']
+console.log(anshuldetails);
+for(let i=1;i<=anshuldetails.length;i++){
+   console.log(i+" "+anshuldetails[i]);
+
+}
