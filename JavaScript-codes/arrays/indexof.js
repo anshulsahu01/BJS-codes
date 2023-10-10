63a830dc05e2fa9a7f36a6f674a528a6d0f8d6e6
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-10 11:21:50 +0530

    indexOf & slice

---
+++ JavaScript-codes/arrays/indexof.js
@@ -0,0 +1,7 @@
+// Indexof 
+//slice
+
+var ar =['anshul','likhit','c','d','e']
+console.log( ar.indexOf('e'))// agr -1 ay to smjh jaoo value ni hai or is mai     casesensitive hai sb
+
+console.log(ar.slice(1,3));// ye us index ke bich ki value print karegi
