44237383abe4280037446ba2b10c5770a3af78b3
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-10-19 00:04:13 +0530

    js

--- JavaScript-codes/assignment/22.js
+++ JavaScript-codes/assignment/22.js
@@ -1,4 +1,4 @@
-//22.	Write a program to find the sum of elements of an array which are multiple of 3 or 5.
+//22.	Write a program to find the sum of elements of an array which are multiple of 3 or 5.  
 
 
 let arr =[1,4,5,78,9,42,51,45,225,22]
